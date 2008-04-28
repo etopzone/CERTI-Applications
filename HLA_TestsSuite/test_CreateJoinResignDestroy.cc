051cd49e5957d82a004f3836a46f63c7f22eea79
Eric Noulard <eric.noulard@gmail.com>
2008-04-28 07:39:57 +0000

    Add test example which may be used as a pattern for other tests

---
+++ HLA_TestsSuite/test_CreateJoinResignDestroy.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <NullFederateAmbassador.hh>
+
+class TestAmbassador : public NullFederateAmbassador
+{
+public:
+  TestAmbassador()
+   : mRtiAmbassador()
+  {}
+  virtual ~TestAmbassador()
+    throw (RTI::FederateInternalError)
+  {}
+
+  int joinAndLeave(const char* federation, const char* model, const char* federate);
+  int destroy(const char* federation);
+
+protected:
+  RTI::RTIambassador mRtiAmbassador;
+};
+
+int TestAmbassador::joinAndLeave(
+  const char* federation, const char* model, const char* federate)
+{
+  try
+  {
+    mRtiAmbassador.createFederationExecution(federation, model);
+  }
+  catch (RTI::FederationExecutionAlreadyExists& e)
+  { }
+
+  mRtiAmbassador.joinFederationExecution(federate, federation, this);
+
+  mRtiAmbassador.resignFederationExecution(RTI::NO_ACTION);
+    //RTI::DELETE_OBJECTS_AND_RELEASE_ATTRIBUTES);
+
+  return 0;
+}
+
+int 
+TestAmbassador::destroy(const char* federation)
+{
+  try
+  {
+    mRtiAmbassador.destroyFederationExecution(federation);
+  }
+  catch (RTI::FederationExecutionDoesNotExist& e)
+  { }
+
+  return 0;
+}
+
+
+int main(void)
+{
+  TestAmbassador test;
+  //test.joinAndLeave("Test01", "Test01.fed", "name");
+  std::cout << "OK" << std::endl;
+  test.joinAndLeave("Test01", "Test01.fed", "");
+  //test.destroy("Test01");
+  std::cout << "OK" << std::endl;
+
+  return 0;
+}
