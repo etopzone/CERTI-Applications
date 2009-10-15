98eaa674b19e35923c854eaea1197d2f1eb6745e
Eric Noulard <eric.noulard@gmail.com>
2009-10-15 20:56:51 +0000

    Update with a new TRTCCallback test. Not finished [yet]

--- HLA_TestsSuite/test_TRTCCallbacks.cc
+++ HLA_TestsSuite/test_TRTCCallbacks.cc
@@ -23,9 +23,11 @@ class TRTCCallbacksFed : public NullFederateAmbassador {
 private:
 	string federationName;
 	string fedFile;
+	string federateName;
 	bool creator;
 	bool TIME_REGULATION_ENABLED, TIME_CONSTRAINED_ENABLED;
 	bool TIME_ADVANCE_GRANT;
+	int nbTARstepsToDo;
 	typedef struct UpdateMeH {
 		RTI::ObjectClassHandle och_updateMe;
 		RTI::AttributeHandle   ah_pingMe;
@@ -34,27 +36,29 @@ private:
 
 	UpdateMeH_t umoh;
 	typedef struct UpdateMeObject {
+		std::string            name;
 		RTI::ObjectHandle      oh_updateMe;
 		libhla::HLAdata<libhla::HLAinteger64LE> pingMe;
 		libhla::HLAdata<libhla::HLAinteger64LE> counter;
 	} UpdateMeObject_t;
 
-	std::vector<UpdateMeObject_t> umo;
+	UpdateMeObject_t myUmo;
+	std::map<RTI::ObjectHandle,UpdateMeObject_t*> otherUmo;
 	RTIfedTime mytime;
 	RTIfedTime myLookahead;
 
 public:
 	TRTCCallbacksFed(void) {
-		UpdateMeObject_t initVal;
 		federationName = "TestTRTCCallbacks";
 		fedFile = "test_TRTCCallbacks.fed";
 		creator = false;
 		TIME_REGULATION_ENABLED = false;
 		TIME_CONSTRAINED_ENABLED = false;
-		umo.reserve(MAX_FEDERATES);
-		*(initVal.pingMe) = 0;
-		*(initVal.counter) = 0;
-		umo[0] = initVal;
+		TIME_ADVANCE_GRANT = false;
+		nbTARstepsToDo = 0;
+		myUmo.name ="unknown";
+		*(myUmo.pingMe) = 0;
+		*(myUmo.counter) = 0;
 		// initial simulation time is 0
 		mytime.setZero();
 		myLookahead.setZero();
@@ -63,7 +67,15 @@ public:
 	};
 
 	virtual ~TRTCCallbacksFed() throw (RTI::FederateInternalError) {
+		for (std::map<RTI::ObjectHandle,UpdateMeObject_t*>::iterator it = otherUmo.begin(); it != otherUmo.end(); it++) {
+			delete (it->second);
+		}
+		otherUmo.clear();
 	};
+
+	void setNbTARStepToDo(int nbsteps) {
+		this->nbTARstepsToDo = nbsteps;
+	}
 
 	int
 	main(void) {
@@ -72,24 +84,96 @@ public:
 		if (creator) {
 			cout << "Press ENTER to start execution ..." << endl;
 			getchar();
-
-			this->enableTimeReg();
-			while (!TIME_REGULATION_ENABLED) this->tickRTI();
-			TIME_REGULATION_ENABLED=false;
-			this->enableTimeCon();
-			while (!TIME_CONSTRAINED_ENABLED) this->tickRTI();
-			TIME_CONSTRAINED_ENABLED=false;
-		} else {
-			this->enableTimeReg();
-			while (!TIME_REGULATION_ENABLED) this->tickRTI();
-			TIME_REGULATION_ENABLED=false;
-			this->enableTimeCon();
-			while (!TIME_CONSTRAINED_ENABLED) this->tickRTI();
-			TIME_CONSTRAINED_ENABLED=false;
 		}
+		this->publishAndRegister();
+
+		this->enableTimeReg();
+		while (!TIME_REGULATION_ENABLED) this->tickRTI();
+		TIME_REGULATION_ENABLED=false;
+		this->enableTimeCon();
+		while (!TIME_CONSTRAINED_ENABLED) this->tickRTI();
+		TIME_CONSTRAINED_ENABLED=false;
+
+		// advance time loop
+		while (nbTARstepsToDo>0) {
+			this->advanceTime(mytime+myLookahead);
+			nbTARstepsToDo--;
+		}
+
 		this->resign_and_destroy();
 		return 0;
 	};
+
+	void
+	discoverObjectInstance (RTI::ObjectHandle theObject,
+			RTI::ObjectClassHandle theObjectClass,
+			const char *theObjectName)
+	throw ( RTI::CouldNotDiscover,
+			RTI::ObjectClassNotKnown,
+			RTI::FederateInternalError) {
+
+		UpdateMeObject_t*  newObject = new UpdateMeObject_t();
+
+		newObject->name = std::string(theObjectName);
+		newObject->oh_updateMe = theObject;
+		otherUmo[newObject->oh_updateMe] = newObject;
+		cout << "Discovered " << newObject->name
+				<< " [handle =" << newObject->oh_updateMe <<"]"<< endl;
+	}
+
+	void
+	reflectAttributeValues (RTI::ObjectHandle theObject,
+			const RTI::AttributeHandleValuePairSet&
+			theAttributes,
+			const char *theTag)
+	throw ( RTI::ObjectNotKnown,
+			RTI::AttributeNotKnown,
+			RTI::FederateOwnsAttributes,
+			RTI::FederateInternalError) {
+
+		RTI::ULong rul;
+		char* value;
+
+		if (theAttributes.getHandle(0)==umoh.ah_counter) {
+			value = theAttributes.getValuePointer(0,rul);
+			//libhla::HLAdata<libhla::HLAinteger64LE> newval(value,rul);
+			(*(otherUmo[theObject]->counter)).copy(value);
+		}
+
+		if (theAttributes.getHandle(0)==umoh.ah_pingMe) {
+			value = theAttributes.getValuePointer(0,rul);
+			//libhla::HLAdata<libhla::HLAinteger64LE> newval(value,rul);
+			(*(otherUmo[theObject]->pingMe)).copy(value);
+		}
+
+		cout << "rav wo ts" << endl;
+	}
+
+	void
+	reflectAttributeValues (RTI::ObjectHandle theObject,
+			const RTI::AttributeHandleValuePairSet&
+			theAttributes,
+			const RTI::FedTime& theTime,
+			const char *theTag,
+			RTI::EventRetractionHandle erhdl)
+	throw ( RTI::ObjectNotKnown,
+			RTI::AttributeNotKnown,
+			RTI::FederateOwnsAttributes,
+			RTI::InvalidFederationTime,
+			RTI::FederateInternalError) {
+
+		cout << "rav w ts " << (static_cast<RTIfedTime>(theTime)).getTime() << endl;
+	}
+
+	void
+	timeAdvanceGrant(const RTI::FedTime &newTime)
+	throw (RTI::InvalidFederationTime,
+			RTI::TimeAdvanceWasNotInProgress,
+			RTI::FederateInternalError) {
+		TIME_ADVANCE_GRANT = true;
+		mytime = newTime;
+		cout << "TIME_ADVANCE_GRANT" << endl;
+	}
 
 private:
 
@@ -109,7 +193,7 @@ private:
 			cerr << "Error: Unknown non-RTI exception." << endl;
 		}
 
-		string federateName = "TRTCCallbacks0";
+		federateName = "TRTCCallbacks0";
 		for (int i=1;i<=MAX_FEDERATES;i++) {
 			cout << "Try to join joining federation: " << i << endl;
 			try {
@@ -199,6 +283,7 @@ private:
 
 		try {
 			rtiamb.publishObjectClass(umoh.och_updateMe, *attributes);
+
 		} catch ( RTI::Exception &e ) {
 			cerr << "RTI exception: " << e._name << " ["
 					<< (e._reason ? e._reason : "undefined") << "]." << endl;
@@ -209,7 +294,8 @@ private:
 
 		// Register My Object instance
 		try {
-			umo[0].oh_updateMe = rtiamb.registerObjectInstance(umoh.och_updateMe);
+			myUmo.name = "UMO-"+federateName;
+			myUmo.oh_updateMe = rtiamb.registerObjectInstance(umoh.och_updateMe);
 		} catch ( RTI::Exception &e ) {
 			cerr << "RTI exception: " << e._name << " ["
 					<< (e._reason ? e._reason : "undefined") << "]." << endl;
@@ -223,20 +309,18 @@ private:
 	 * @param ts true if the UAV is timestamped false otherwise
 	 */
 	void UAV(bool ts) {
-
-
 		auto_ptr<RTI::AttributeHandleValuePairSet>
 
 		ahvps(RTI::AttributeSetFactory::create(2));
-		ahvps->add(umoh.ah_counter, umo[0].counter.data(), umo[0].counter.size());
-		ahvps->add(umoh.ah_pingMe, umo[0].pingMe.data(), umo[0].pingMe.size());
+		ahvps->add(umoh.ah_counter, myUmo.counter.data(), myUmo.counter.size());
+		ahvps->add(umoh.ah_pingMe, myUmo.pingMe.data(), myUmo.pingMe.size());
 
 		try {
 			if (ts) {
-				rtiamb.updateAttributeValues(umo[0].oh_updateMe, *ahvps,mytime+myLookahead, "upd");
+				rtiamb.updateAttributeValues(myUmo.oh_updateMe, *ahvps,mytime+myLookahead, "upd");
 			}
 			else {
-				rtiamb.updateAttributeValues(umo[0].oh_updateMe, *ahvps,"upd");
+				rtiamb.updateAttributeValues(myUmo.oh_updateMe, *ahvps,"upd");
 			}
 		} catch ( RTI::Exception &e ) {
 			cerr << "RTI exception: " << e._name << " ["
@@ -297,9 +381,9 @@ private:
 	}
 
 	void
-	advanceTime(double time) {
+	advanceTime(const RTIfedTime& time) {
 		try {
-			rtiamb.timeAdvanceRequest(RTIfedTime(time));
+			rtiamb.timeAdvanceRequest(time);
 		} catch ( RTI::Exception &e ) {
 			cerr << "RTI exception: " << e._name << " ["
 					<< (e._reason ? e._reason : "undefined") << "]." << endl;
@@ -308,6 +392,7 @@ private:
 		}
 		while (!TIME_ADVANCE_GRANT) this->tickRTI();
 		TIME_ADVANCE_GRANT = false;
+		getchar();
 	}
 
 	/* Federate Ambassador services */
@@ -339,5 +424,9 @@ protected:
 
 int main(int argc, char **argv) {
 	TRTCCallbacksFed testFed;
+	if (argc>1) {
+		testFed.setNbTARStepToDo(atoi(argv[1]));
+	}
+
 	return testFed.main();
 }
