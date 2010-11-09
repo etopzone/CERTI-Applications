88687dc4e0d209a54fb94aa027249502f6358e1f
Eric Noulard <eric.noulard@gmail.com>
2010-11-09 12:47:04 +0000

    Add LevelOne.fed parse test.

--- HLA_TestsSuite/dtest_test_FOMParse.py
+++ HLA_TestsSuite/dtest_test_FOMParse.py
@@ -93,6 +93,9 @@ FOMFed8 = dtest.DTester("test_FOMParse-BuggyFed",
 FOMFed9 = dtest.DTester("test_FOMParse-portico-test",
                         session=dtest.SSHSessionHandler(federate_param['user'],host=federate_param['host']))
 
+FOMFed10 = dtest.DTester("test_FOMParse-LevelOne",
+                        session=dtest.SSHSessionHandler(federate_param['user'],host=federate_param['host']))
+
 
 # you may change the default time out value
 rtig.timeout = 40
@@ -149,9 +152,10 @@ addFOMFedSequence(True,FOMFed4, " -v -f aviationsimnetV3.1.xml -n AviationSimNet
 addFOMFedSequence(True,FOMFed5, " -v -f aviationsimnetV3.1.xml -n AviationSimNet -j FOMFed5 -o Manager.Federate -a FederateType -i Manager.Federate -p Federate","FOM4","FOM5")
 addFOMFedSequence(True,FOMFed6, " -v -f Hierarchical.fed -n Hierarchical -j FOMFed6 -o L2 -a attL1_1 -i IL2 -p paramIL1_1","FOM5","FOM6")
 addFOMFedSequence(True,FOMFed7, " -v -f Hierarchical.fed -n Hierarchical -j FOMFed6 -o L1.L2 -a attL1_1 -i IL1.IL2 -p paramIL1_1","FOM6","FOM7")
-addFOMFedSequence(False,FOMFed8, " -v -f BuggyFed.fed -n BuggyFed","FOM7","All Federate(s) ended")
-#addFOMFedSequence(False,FOMFed8, " -v -f BuggyFed.fed -n BuggyFed","FOM7","FOM8")
-#addFOMFedSequence(True,FOMFed9, " -v -f portico-testfom.fed -n Portico-Test","FOM8","All Federate(s) ended")
+#addFOMFedSequence(False,FOMFed8, " -v -f BuggyFed.fed -n BuggyFed","FOM7","All Federate(s) ended")
+addFOMFedSequence(False,FOMFed8, " -v -f BuggyFed.fed -n BuggyFed","FOM7","FOM8")
+addFOMFedSequence(True,FOMFed9, " -v -f portico-testfom.fed -n Portico-Test","FOM8","FOM9")
+addFOMFedSequence(True,FOMFed10, " -v -f LevelOne.fed -n LevelOne","FOM9","All Federate(s) ended")
 
 def goTest():
     myDTestMaster = dtest.DTestMaster("HLA test test_FOMParse Starts","Launch RTIG + several test_FOMParse federates for testing several FOM file parsing.")
@@ -165,8 +169,9 @@ def goTest():
     myDTestMaster.register(FOMFed5)    
     myDTestMaster.register(FOMFed6)
     myDTestMaster.register(FOMFed7)
-    myDTestMaster.register(FOMFed8)
-#    myDTestMaster.register(FOMFed9)        
+    myDTestMaster.register(FOMFed8)    
+    myDTestMaster.register(FOMFed9)
+    myDTestMaster.register(FOMFed10)        
     myDTestMaster.startTestSequence()
     myDTestMaster.waitTestSequenceEnd()
     
